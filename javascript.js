74d3d5502655b34f4dc380ec0b2f5ca8442d54e8
phillipe henriques <phillipe_ch@hotmail.com>
2022-08-03 00:03:21 -0300

    hover aplicado

--- javascript.js
+++ javascript.js
@@ -9,5 +9,24 @@ function trocar(id) {
     maior.innerHTML = '<img class="img-bm" src="imagens/'+ id +'.jpg" alt=""/>'
   }
 
-
 }
+
+// JQUERY EFFECT <<
+
+$(document).ready(function() {
+  $(".column img").click(function() {
+    console.clear();
+    var newclass = $(this).attr("src");
+    console.log(newclass);
+    var oldclass = $("#expandedImg").attr("id");
+    console.log(oldclass);
+
+    $(".container").show();
+    // show .container
+
+    $("#expandedImg").attr('src', newclass).hide().fadeIn("slow");
+    //set new source and hide element in order to be able to fade it in again
+    // fade in works only on hidden elements
+
+  });
+});
